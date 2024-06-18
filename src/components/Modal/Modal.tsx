189cd40b67b9ed7070402d4cfc42b6e1891f19b4
uaquax <uaquax@tutanota.com>
2024-06-18 14:25:08 +0800

    login

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,10 +4,14 @@ import s from "./Modal.module.css";
 import { closeModal } from "../../redux/slices/modalSlice";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import Logo from "../../assets/modal/logo.svg";
+import { useState } from "react";
+import RuFlag from "../../assets/ru_flag.svg";
 
 const Modal = () => {
   const isOpen = useAppSelector((state) => state.modal.isOpened);
   const dispatch = useDispatch();
+  const [phone, setPhone] = useState("+7");
+  const numberReg = /^[+]?(\d+)?$/;
 
   function handleClick(e: React.MouseEvent<HTMLElement>) {
     if ((e.target as HTMLElement).id === "modal") {
@@ -34,6 +38,28 @@ const Modal = () => {
               Код будет автоматически отправлен на Ваш номер телефона с помощью
               SMS
             </p>
+          </div>
+
+          <div className={s.phoneInput}>
+            <label htmlFor="phoneInput">Номер телефона</label>
+            <div className={s.input}>
+              <img src={RuFlag} className={s.flag} />
+              <input
+                id="phoneInput"
+                value={phone}
+                onChange={(e) => {
+                  if (e.target.value.length <= 0) return;
+                  if (!e.target.value.match(numberReg)) return;
+
+                  setPhone(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <button className={s.sendBtn}>Получить код</button>
+          <div className={s.registration}>
+            <p className={s.noAccountText}>Нет аккаунта?</p>
+            <button className={s.registerBtn}>Зарегистрироваться</button>
           </div>
         </div>
       </div>
