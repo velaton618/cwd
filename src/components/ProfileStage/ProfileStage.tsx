a52e0da7a0ebbf3fdbe10066cbb2a5b6f9d6c8b7
uaquax <uaquax@tutanota.com>
2024-06-19 18:03:32 +0800

    file select on avatar

--- src/components/ProfileStage/ProfileStage.tsx
+++ src/components/ProfileStage/ProfileStage.tsx
@@ -1,7 +1,25 @@
+import { useRef } from "react";
 import s from "./ProfileStage.module.scss";
 
 const ProfileStage = () => {
-  return <div>ProfileStage</div>;
+  const fileInput = useRef<HTMLInputElement | null>(null);
+
+  const handleAvatarClick = () => {
+    fileInput?.current?.click();
+  };
+
+  return (
+    <div className={s.container}>
+      <input type="file" ref={fileInput} style={{ display: "none" }} />
+      <img
+        src="/default_user.png"
+        className={s.avatar}
+        onClick={handleAvatarClick}
+      />
+
+      <div className={s.fields}></div>
+    </div>
+  );
 };
 
 export default ProfileStage;
