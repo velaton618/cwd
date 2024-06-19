78ed8789268f7862b103b4d1bd6b1ce69cf4c411
uaquax <uaquax@tutanota.com>
2024-06-19 12:40:03 +0800

    readme

--- src/components/CodeText/CodeText.tsx
+++ src/components/CodeText/CodeText.tsx
@@ -1,11 +1,14 @@
+import { useAppSelector } from "../../redux/hooks";
 import s from "./CodeText.module.scss";
 
 const CodeText = () => {
+  const phone = useAppSelector((state) => state.modal.phone);
+
   return (
     <div className={s.text}>
       <h1 className={s.title}>Подтвердите номер телефона</h1>
       <p className={s.description}>
-        Отправим сообщение с 6-ти значным кодом на номер +7 983 540-49-43
+        Отправим сообщение с 6-ти значным кодом на номер {phone}
         <button className={s.changeBtn}>Изменить</button>
       </p>
     </div>
