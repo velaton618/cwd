6e4fcbf0f9c49a32a41d92fa4e6cb63f0f5c98bd
uaquax <uaquax@tutanota.com>
2024-06-19 16:14:30 +0800

    sports choice images

--- src/components/SportsChoice/SportsChoice.tsx
+++ src/components/SportsChoice/SportsChoice.tsx
@@ -1,14 +1,93 @@
+import { useState } from "react";
 import s from "./SportsChoice.module.scss";
 
+interface Selected {
+  cycling: boolean;
+  handball: boolean;
+  skating: boolean;
+  sambo: boolean;
+  wrestling: boolean;
+  weightlifting: boolean;
+}
+
 const SportsChoice = () => {
+  const [selected, setSelected] = useState<Selected>({
+    cycling: false,
+    handball: false,
+    skating: false,
+    sambo: false,
+    wrestling: false,
+    weightlifting: false,
+  });
+
   return (
     <div className={s.container}>
-      <div className={s.block}>1</div>
-      <div className={s.block}>2</div>
-      <div className={s.block}>3</div>
-      <div className={s.block}>4</div>
-      <div className={s.block}>5</div>
-      <div className={s.block}>6</div>
+      <div
+        className={`${s.block} ${selected.cycling && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            cycling: !selected.cycling,
+          });
+        }}
+      >
+        <img src="sports/cycling.png" />
+      </div>
+      <div
+        className={`${s.block} ${selected.handball && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            handball: !selected.handball,
+          });
+        }}
+      >
+        <img src="sports/handball.png" />
+      </div>
+      <div
+        className={`${s.block} ${selected.skating && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            skating: !selected.skating,
+          });
+        }}
+      >
+        <img src="sports/skating.png" />
+      </div>
+      <div
+        className={`${s.block} ${selected.sambo && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            sambo: !selected.skating,
+          });
+        }}
+      >
+        <img src="sports/sambo.png" />
+      </div>
+      <div
+        className={`${s.block} ${selected.wrestling && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            wrestling: !selected.wrestling,
+          });
+        }}
+      >
+        <img src="sports/wrestling.png" />
+      </div>
+      <div
+        className={`${s.block} ${selected.weightlifting && s.active}`}
+        onClick={() => {
+          setSelected({
+            ...selected,
+            weightlifting: !selected.weightlifting,
+          });
+        }}
+      >
+        <img src="sports/weightlifting.png" />
+      </div>
     </div>
   );
 };
